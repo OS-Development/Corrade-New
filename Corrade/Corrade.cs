c9cd53ef1a6516bf6cc30bdc944b788765b0ff49
vero <vero@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2014-11-10 17:16:10 +0000

    Fix copyright year.

--- Corrade/Corrade.cs
+++ Corrade/Corrade.cs
@@ -1,5 +1,5 @@
 ﻿///////////////////////////////////////////////////////////////////////////
-//  Copyright (C) Wizardry and Steamworks 2014 - License: GNU GPLv3      //
+//  Copyright (C) Wizardry and Steamworks 2013 - License: GNU GPLv3      //
 //  Please see: http://www.gnu.org/licenses/gpl.html for legal details,  //
 //  rights of fair usage, the disclaimer and warranty conditions.        //
 ///////////////////////////////////////////////////////////////////////////
--- Corrade/Properties/AssemblyInfo.cs
+++ Corrade/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.18.*")]
+[assembly: AssemblyVersion("7.19.*")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
