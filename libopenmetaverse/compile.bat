565ce4e39514cb076fd1e5f0ab90f1e5c8d7718d
eva <eva@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2015-02-26 19:08:35 +0000

    - Release 7.74 - added AIML artificial intelligence via the "AI" command.

--- libopenmetaverse/compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild OpenMetaverse.sln 
-:SUCCESS 
-echo Build Successful! 
-exit /B 0 
-:FAIL 
-echo Build Failed, check log for reason 
-exit /B 1 
