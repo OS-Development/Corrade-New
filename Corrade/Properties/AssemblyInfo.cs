a47f2b91ba1436eecd6d02d712eb5b3151ce5d9f
eva <eva@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2015-04-30 21:12:24 +0000

    Release 7.115 - this release marks the end of Corrade 7 (at revision 116). - Remove dynamic event handlers and treat them separately in the configuration. - Eliminated a SEGV stemming from early logins before structures were initialized.

--- Corrade/Properties/AssemblyInfo.cs
+++ Corrade/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.114.*")]
+[assembly: AssemblyVersion("7.115.*")]
 [assembly: NeutralResourcesLanguage("en")]
