80f5b2ac79ec0fa1f197fcefb9a47c5880ce67eb
eva <eva@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2015-08-20 12:02:34 +0000

    - Release 8.43 - feature additions. - Added default throttles to "Corrade.ini" where you can change the libopenmetaverse throttles for various parts of the library. - Corrade now additionally returns a "status" along with "error" and "success" in case a command failed. The new "status" key now contains an unique error code that should make branching easier from scripts. Please see the status codes section for the mapping of status codes to error descriptions. - Fixed a small problem with primitive and avatar detection since the previous versions only returned primitives and avatars from the current region in case the range was smaller than the draw distance. - Added a command "creategrass" that creates foilage at a specified location provided that Corrade is the owner of the parcel (Second Life limitation). - The "setprimitivetexturedata" command has been altered to take "face" instead of "index" as parameter and accept "all" and "default" as values which will set the texture data for all faces, respectively the default texture. - Added the "getprimitivebodytypes" command that retrieves the bodies that are built-in and can be supplied to the "type" parameter of commands such as "createprimitive" or "setprimitiveshape". - Added the "getstatus" command with an "entity" parameter that must be set to "description" in order to retrieve the description of an error status.

--- Corrade/Properties/AssemblyInfo.cs
+++ Corrade/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("8.42.*")]
+[assembly: AssemblyVersion("8.43.*")]
 [assembly: NeutralResourcesLanguage("en")]
