5532373362f4c7f13433fb67b6976f652ceadc0b
eva <eva@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2016-06-01 07:26:58 +0000

    - Re-add "getgroupmemberdata".

---
+++ Corrade/commands/Corrade/GetGroupMemberData.cs
@@ -0,0 +1,106 @@
+﻿///////////////////////////////////////////////////////////////////////////
+//  Copyright (C) Wizardry and Steamworks 2013 - License: GNU GPLv3      //
+//  Please see: http://www.gnu.org/licenses/gpl.html for legal details,  //
+//  rights of fair usage, the disclaimer and warranty conditions.        //
+///////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using CorradeConfiguration;
+using OpenMetaverse;
+using wasOpenMetaverse;
+using wasSharp;
+
+namespace Corrade
+{
+    public partial class Corrade
+    {
+        public partial class CorradeCommands
+        {
+            public static Action<CorradeCommandParameters, Dictionary<string, string>> getgroupmemberdata =
+                (corradeCommandParameters, result) =>
+                {
+                    if (
+                        !HasCorradePermission(corradeCommandParameters.Group.UUID,
+                            (int)Configuration.Permissions.Interact))
+                    {
+                        throw new ScriptException(ScriptError.NO_CORRADE_PERMISSIONS);
+                    }
+                    UUID groupUUID;
+                    string target = wasInput(
+                        KeyValue.Get(
+                            wasOutput(Reflection.GetNameFromEnumValue(ScriptKeys.TARGET)),
+                            corradeCommandParameters.Message));
+                    switch (string.IsNullOrEmpty(target))
+                    {
+                        case false:
+                            if (!UUID.TryParse(target, out groupUUID) &&
+                                !Resolvers.GroupNameToUUID(Client, target, corradeConfiguration.ServicesTimeout,
+                                    corradeConfiguration.DataTimeout,
+                                    new Time.DecayingAlarm(corradeConfiguration.DataDecayType), ref groupUUID))
+                                throw new ScriptException(ScriptError.GROUP_NOT_FOUND);
+                            break;
+                        default:
+                            groupUUID = corradeCommandParameters.Group.UUID;
+                            break;
+                    }
+                    UUID agentUUID;
+                    if (
+                        !UUID.TryParse(
+                            wasInput(KeyValue.Get(
+                                wasOutput(Reflection.GetNameFromEnumValue(ScriptKeys.AGENT)),
+                                corradeCommandParameters.Message)),
+                            out agentUUID) && !Resolvers.AgentNameToUUID(Client,
+                                wasInput(
+                                    KeyValue.Get(
+                                        wasOutput(Reflection.GetNameFromEnumValue(ScriptKeys.FIRSTNAME)),
+                                        corradeCommandParameters.Message)),
+                                wasInput(
+                                    KeyValue.Get(
+                                        wasOutput(Reflection.GetNameFromEnumValue(ScriptKeys.LASTNAME)),
+                                        corradeCommandParameters.Message)),
+                                corradeConfiguration.ServicesTimeout, corradeConfiguration.DataTimeout,
+                                new Time.DecayingAlarm(corradeConfiguration.DataDecayType),
+                                ref agentUUID))
+                    {
+                        throw new ScriptException(ScriptError.AGENT_NOT_FOUND);
+                    }
+                    Dictionary<UUID, GroupMember> groupMembers = null;
+                    ManualResetEvent groupMembersReceivedEvent = new ManualResetEvent(false);
+                    EventHandler<GroupMembersReplyEventArgs> GroupMembersReplyEventHandler = (sender, args) =>
+                    {
+                        groupMembers = args.Members;
+                        groupMembersReceivedEvent.Set();
+                    };
+                    lock (Locks.ClientInstanceGroupsLock)
+                    {
+                        Client.Groups.GroupMembersReply += GroupMembersReplyEventHandler;
+                        Client.Groups.RequestGroupMembers(groupUUID);
+                        if (
+                            !groupMembersReceivedEvent.WaitOne(
+                                (int)corradeConfiguration.ServicesTimeout, false))
+                        {
+                            Client.Groups.GroupMembersReply -= GroupMembersReplyEventHandler;
+                            throw new ScriptException(ScriptError.TIMEOUT_GETTING_GROUP_MEMBERS);
+                        }
+                        Client.Groups.GroupMembersReply -= GroupMembersReplyEventHandler;
+                    }
+                    GroupMember groupMember;
+                    if (!groupMembers.TryGetValue(agentUUID, out groupMember))
+                    {
+                        throw new ScriptException(ScriptError.AGENT_NOT_FOUND);
+                    }
+                    List<string> data = GetStructuredData(groupMember,
+                        wasInput(KeyValue.Get(wasOutput(Reflection.GetNameFromEnumValue(ScriptKeys.DATA)),
+                            corradeCommandParameters.Message))).ToList();
+                    if (data.Any())
+                    {
+                        result.Add(Reflection.GetNameFromEnumValue(ResultKeys.DATA),
+                            CSV.FromEnumerable(data));
+                    }
+                };
+        }
+    }
+}
