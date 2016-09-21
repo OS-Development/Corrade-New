fbd631c202102fc156dbcdd3727ca8f6ba947a6b
vero <vero@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2016-09-21 08:27:54 +0000

    - Add preload.

---
+++ Corrade/notifications/Preload.cs
@@ -0,0 +1,47 @@
+﻿///////////////////////////////////////////////////////////////////////////
+//  Copyright (C) Wizardry and Steamworks 2013 - License: GNU GPLv3      //
+//  Please see: http://www.gnu.org/licenses/gpl.html for legal details,  //
+//  rights of fair usage, the disclaimer and warranty conditions.        //
+///////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Corrade.Helpers;
+using OpenMetaverse;
+using wasSharp;
+
+namespace Corrade
+{
+    public partial class Corrade
+    {
+        public static partial class CorradeNotifications
+        {
+            public static Action<NotificationParameters, Dictionary<string, string>> preload =
+                (corradeNotificationParameters, notificationData) =>
+                {
+                    var preloadSoundEventArgs =
+                        (PreloadSoundEventArgs)corradeNotificationParameters.Event;
+                    // In case we should send specific data then query the structure and return.
+                    if (corradeNotificationParameters.Notification.Data != null &&
+                        corradeNotificationParameters.Notification.Data.Any())
+                    {
+                        notificationData.Add(Reflection.GetNameFromEnumValue(Command.ScriptKeys.DATA),
+                            CSV.FromEnumerable(wasOpenMetaverse.Reflection.GetStructuredData(preloadSoundEventArgs,
+                                CSV.FromEnumerable(corradeNotificationParameters.Notification.Data))));
+                        return;
+                    }
+
+                    var LockObject = new object();
+                    Notifications.LoadSerializedNotificationParameters(corradeNotificationParameters.Type)
+                        .NotificationParameters.AsParallel()
+                        .ForAll(o => o.Value.AsParallel().ForAll(p =>
+                        {
+                            p.ProcessParameters(Client, corradeConfiguration, o.Key,
+                                new List<object> { preloadSoundEventArgs },
+                                notificationData, LockObject, rankedLanguageIdentifier);
+                        }));
+                };
+        }
+    }
+}
