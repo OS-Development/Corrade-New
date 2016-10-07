cede7e725163ba9ace8d764f1be601a3c620c7bd
eva <eva@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2016-10-07 14:16:57 +0000

    - Commit safe file stream mutex for the future. - Commit process for the future.

---
+++ wasSharpNET/IO/SafeFileStream.cs
@@ -0,0 +1,110 @@
+﻿///////////////////////////////////////////////////////////////////////////
+//  Copyright (C) Wizardry and Steamworks 2016 - License: GNU GPLv3      //
+//  Please see: http://www.gnu.org/licenses/gpl.html for legal details,  //
+//  rights of fair usage, the disclaimer and warranty conditions.        //
+///////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+
+namespace wasSharpNET.IO
+{
+    /// <summary>
+    ///     This is a wrapper aroung a FileStream.  While it is not a Stream itself, it can be cast to
+    ///     one (keep in mind that this might throw an exception).
+    /// </summary>
+    public class SafeFileStream : IDisposable
+    {
+        #region Constructors
+
+        public SafeFileStream(string path, FileMode mode, FileAccess access, FileShare share, uint milliscondsTimeout)
+        {
+            m_mutex = new Mutex(false, string.Format("Global\\{0}", path.Replace('\\', '/')));
+            m_path = path;
+            m_fileMode = mode;
+            m_fileAccess = access;
+            m_fileShare = share;
+            m_millisecondsTimeout = milliscondsTimeout;
+        }
+
+        #endregion//Constructors
+
+        #region Private Members
+
+        private readonly Mutex m_mutex;
+        private Stream m_stream;
+        private readonly string m_path;
+        private readonly FileMode m_fileMode;
+        private readonly FileAccess m_fileAccess;
+        private readonly FileShare m_fileShare;
+        private readonly uint m_millisecondsTimeout;
+
+        #endregion//Private Members
+
+        #region Properties
+
+        public Stream Stream
+        {
+            get
+            {
+                if (!IsOpen && !TryOpen(TimeSpan.FromMilliseconds(m_millisecondsTimeout)))
+                    throw new InvalidOperationException("Timeout opening stream.");
+                return m_stream;
+            }
+        }
+
+        private bool IsOpen => m_stream != null;
+
+        #endregion//Properties
+
+        #region Functions
+
+        /// <summary>
+        ///     Opens the stream when it is not locked.  If the file is locked, then
+        /// </summary>
+        public void Open()
+        {
+            if (m_stream != null)
+                throw new InvalidOperationException("The stream is already open.");
+            m_mutex.WaitOne();
+            m_stream = File.Open(m_path, m_fileMode, m_fileAccess, m_fileShare);
+        }
+
+        public bool TryOpen(TimeSpan span)
+        {
+            if (m_stream != null)
+                throw new InvalidOperationException("The stream is already open.");
+            if (m_mutex.WaitOne(span))
+            {
+                m_stream = File.Open(m_path, m_fileMode, m_fileAccess, m_fileShare);
+                return true;
+            }
+            return false;
+        }
+
+        public void Close()
+        {
+            if (m_stream == null)
+                return;
+            m_stream.Close();
+            m_stream = null;
+            m_mutex.ReleaseMutex();
+        }
+
+        public void Dispose()
+        {
+            Close();
+            GC.SuppressFinalize(this);
+        }
+
+        public static implicit operator Stream(SafeFileStream sfs)
+        {
+            return sfs.Stream;
+        }
+
+        #endregion//Functions
+    }
+}
---
+++ wasSharpNET/Process.cs
@@ -0,0 +1,37 @@
+﻿///////////////////////////////////////////////////////////////////////////
+//  Copyright (C) Wizardry and Steamworks 2016 - License: GNU GPLv3      //
+//  Please see: http://www.gnu.org/licenses/gpl.html for legal details,  //
+//  rights of fair usage, the disclaimer and warranty conditions.        //
+///////////////////////////////////////////////////////////////////////////
+
+using System.Diagnostics;
+
+namespace wasSharpNET
+{
+    public static class Process
+    {
+        /// <summary>
+        ///     Retrieves the instance name from a processr.
+        /// </summary>
+        /// <param name="proc">the process</param>
+        /// <returns>the instance name of a process or null if the instance name was not found</returns>
+        /// <remarks>(C) Ingo Rammer</remarks>
+        public static string GetProcessInstanceName(this System.Diagnostics.Process proc)
+        {
+            var cat = new PerformanceCounterCategory("Process");
+            
+            foreach (var instance in cat.GetInstanceNames())
+            {
+                using (var cnt = new PerformanceCounter("Process", "ID Process", instance, true))
+                {
+                    var val = (int) cnt.RawValue;
+                    if (val == proc.Id)
+                    {
+                        return instance;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
