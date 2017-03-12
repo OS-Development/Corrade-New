0de2fb5c68eba534d0ccc6223b704b64b51bf1ca
office <office@b192ff8a-d217-4b8e-9fb6-9364795ca931>
2017-03-12 13:49:24 +0000

    - Header fix.

--- Corrade/Source/WebForms/SecondLife/Statement.cs
+++ Corrade/Source/WebForms/SecondLife/Statement.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿///////////////////////////////////////////////////////////////////////////
+//  Copyright (C) Wizardry and Steamworks 2017 - License: GNU GPLv3      //
+//  Please see: http://www.gnu.org/licenses/gpl.html for legal details,  //
+//  rights of fair usage, the disclaimer and warranty conditions.        //
+///////////////////////////////////////////////////////////////////////////
 
 namespace Corrade.Source.WebForms.SecondLife
 {
